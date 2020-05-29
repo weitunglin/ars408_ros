0b70254105e7507ce522b7f97e31f4d1adf37865
vip_laptop <vip_laptop@mail.ntust.edu.tw>
2020-05-29 12:00:43 +0800

    Update toViedo.py.

--- ars408_ros/scripts/toVideo.py
+++ ars408_ros/scripts/toVideo.py
@@ -4,27 +4,36 @@ from sensor_msgs.msg import Image
 from std_msgs.msg import String
 from cv_bridge import CvBridge
 import cv2
+import os
 
-# topic = "/rgbImg"
-topic = "/thermalImg"
+topic_RGB = "/rgbImg"
+topic_TRM = "/thermalImg"
 
-# path = "/home/viplab/rgb_"
-path = "/home/viplab/thermal_"
+path_RGB = os.path.expanduser("~/rgb_")
+path_TRM = os.path.expanduser("~/thermal_")
 
-# size = (800,600) #rgb
-size = (640, 512) #thermal
+size_RGB = (800, 600)
+size_TRM = (640, 512)
 
-def callbackImg(data):
+def callback_RGBImg(data):
     bridge = CvBridge()
     img = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
-    global out
-    out.write(img)
+    global out_RGB
+    out_RGB.write(img)
+
+def callback_TRMImg(data):
+    bridge = CvBridge()
+    img = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+    global out_TRM
+    out_TRM.write(img)
     
 def listener():
     rospy.init_node("toVideo", anonymous=False)
-    global out
-    out = cv2.VideoWriter(path+str(rospy.Time.now())+".avi", cv2.VideoWriter_fourcc(*'DIVX'), 20, size, True)
-    sub2 = rospy.Subscriber(topic, Image, callbackImg)
+    global out_RGB, out_TRM
+    out_RGB = cv2.VideoWriter(path_RGB+str(rospy.Time.now())+".avi", cv2.VideoWriter_fourcc(*'DIVX'), 20, size_RGB, True)
+    out_TRM = cv2.VideoWriter(path_TRM+str(rospy.Time.now())+".avi", cv2.VideoWriter_fourcc(*'DIVX'), 20, size_TRM, True)
+    sub_RGB = rospy.Subscriber(topic_RGB, Image, callback_RGBImg)
+    sub_TRM = rospy.Subscriber(topic_TRM, Image, callback_TRMImg)
     rospy.spin()
 
 if __name__ == "__main__":
