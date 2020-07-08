7160f270517101937a1393620ce1aa3c4cb7ee30
YoCheng <xyox860224@gmail.com>
2020-07-08 18:27:01 +0800

    Change method of saving video .

--- ars408_ros/scripts/toVideo.py
+++ ars408_ros/scripts/toVideo.py
@@ -8,11 +8,16 @@ import os
 import argparse
 import time
 
+frameRate = 20
+
 topic_RGB = "/rgbImg"
 topic_TRM = "/thermalImg"
 
 size_RGB = (800, 600)
 size_TRM = (640, 512)
+
+global nowImg_RGB
+global nowImg_TRM
 
 root = os.getcwd()
 aviPath_RGB = os.path.join(root, "RGB_"+str(time.time())+".avi" )
@@ -26,31 +31,38 @@ i_TRM = 0
 def callback_RGBImg(data):
     bridge = CvBridge()
     img = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
-    global out_RGB
-    out_RGB.write(img)
-    if savePic:
-        global i_RGB
-        cv2.imwrite(os.path.join(jpgRoot_RGB, "{0:07}.jpg".format(i_RGB)), img)
-        i_RGB +=1
+    global nowImg_RGB
+    nowImg_RGB = img.copy()
 
 def callback_TRMImg(data):
     bridge = CvBridge()
     img = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
-    global out_TRM
-    out_TRM.write(img)
-    if savePic:
-        global i_TRM
-        cv2.imwrite(os.path.join(jpgRoot_TRM, "{0:07}.jpg".format(i_TRM)), img)
-        i_TRM +=1
+    global nowImg_TRM
+    nowImg_TRM = img.copy()
     
 def listener():
     rospy.init_node("toVideo", anonymous=False)
+    rate = rospy.Rate(frameRate)
+    sub_RGB = rospy.Subscriber(topic_RGB, Image, callback_RGBImg, queue_size=1)
+    sub_TRM = rospy.Subscriber(topic_TRM, Image, callback_TRMImg, queue_size=1)
+
     global out_RGB, out_TRM
-    out_RGB = cv2.VideoWriter(aviPath_RGB, cv2.VideoWriter_fourcc(*'DIVX'), 20, size_RGB, True)
-    out_TRM = cv2.VideoWriter(aviPath_TRM, cv2.VideoWriter_fourcc(*'DIVX'), 20, size_TRM, True)
-    sub_RGB = rospy.Subscriber(topic_RGB, Image, callback_RGBImg)
-    sub_TRM = rospy.Subscriber(topic_TRM, Image, callback_TRMImg)
-    rospy.spin()
+    out_RGB = cv2.VideoWriter(aviPath_RGB, cv2.VideoWriter_fourcc(*'DIVX'), frameRate, size_RGB, True)
+    out_TRM = cv2.VideoWriter(aviPath_TRM, cv2.VideoWriter_fourcc(*'DIVX'), frameRate, size_TRM, True)
+
+    i = 0
+    while not rospy.is_shutdown():
+        if not ("nowImg_RGB"  in globals() and "nowImg_TRM"  in globals()):
+            continue
+    
+        out_RGB.write(nowImg_RGB)
+        out_TRM.write(nowImg_TRM)
+        if savePic:
+            cv2.imwrite(os.path.join(jpgRoot_RGB, "{0:07}.jpg".format(i)), nowImg_RGB)
+            cv2.imwrite(os.path.join(jpgRoot_TRM, "{0:07}.jpg".format(i)), nowImg_TRM)
+            i += 1
+
+        rate.sleep()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Export avi and jpg')
@@ -59,7 +71,7 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--storePic", action="store_true", help="Store pic.")
 
     args = parser.parse_args()
-    root = args.r
+    root = os.path.expanduser(args.r)
 
     if args.o:
         aviPath_RGB = os.path.join(root, "RGB_"+args.o+".avi" )
@@ -71,11 +83,13 @@ if __name__ == "__main__":
 
     if args.storePic:
         savePic = True
-        os.mkdir(jpgRoot_RGB)
-        os.mkdir(jpgRoot_TRM)
+        try:
+            os.makedirs(jpgRoot_RGB)
+            os.makedirs(jpgRoot_TRM)
+        except Exception:
+            pass
 
     try:
         listener()
     except rospy.ROSInternalException:
         pass
-    
