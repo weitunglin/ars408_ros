521bd3338a15f7dda5b0e3e4feb382c524b5a741
vip_laptop <vip@mail.ntust.edu.tw>
2021-03-24 19:06:20 +0800

    update for MultiRGB

--- ars408_ros/scripts/output/toVideoSingleRGB.py
+++ ars408_ros/scripts/output/toVideoSingleRGB.py
@@ -21,27 +21,19 @@ with open(os.path.expanduser("~") + "/catkin_ws/src/ARS408_ros/ars408_ros/config
 
 frameRate = config['frameRate']
 
-topic_RGB1 = "/first/rgbImg"
-topic_RGB2 = "/second/rgbImg"
-topic_RGB3 = "/third/rgbImg"
+topic_RGB1 = "/rgbImg"
 
 size_RGB = config['size_RGB_720p']
 
 global nowImg_RGB1
-global nowImg_RGB2
-global nowImg_RGB3
 
 root = os.getcwd()
 aviPath_RGB1 = os.path.join(root, "RGB1_"+str(time.time())+".avi" )
-aviPath_RGB2 = os.path.join(root, "RGB2_"+str(time.time())+".avi" )
-aviPath_RGB3 = os.path.join(root, "RGB3_"+str(time.time())+".avi" )
+
 savePic = False
 jpgRoot_RGB1 = os.path.join(root, "IMG_RGB1_"+str(time.time()))
-jpgRoot_RGB2 = os.path.join(root, "IMG_RGB2_"+str(time.time()))
-jpgRoot_RGB3 = os.path.join(root, "IMG_RGB3_"+str(time.time()))
+
 i_RGB1 = 0
-i_RGB2 = 0
-i_RGB3 = 0
 
 
 def callback_RGBImg1(data):
@@ -50,43 +42,23 @@ def callback_RGBImg1(data):
     global nowImg_RGB1
     nowImg_RGB1 = img.copy()
 
-def callback_RGBImg2(data):
-    bridge = CvBridge()
-    img = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
-    global nowImg_RGB2
-    nowImg_RGB2 = img.copy()
-
-def callback_RGBImg3(data):
-    bridge = CvBridge()
-    img = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
-    global nowImg_RGB3
-    nowImg_RGB3 = img.copy()
-
 
 def listener():
     rospy.init_node("toVideo")
     rate = rospy.Rate(frameRate)
     sub_RGB1 = rospy.Subscriber(topic_RGB1, Image, callback_RGBImg1, queue_size=1)
-    sub_RGB2 = rospy.Subscriber(topic_RGB2, Image, callback_RGBImg2, queue_size=1)
-    sub_RGB3 = rospy.Subscriber(topic_RGB3, Image, callback_RGBImg3, queue_size=1)
 
-    global out_RGB1, out_RGB2, out_RGB3
+    global out_RGB1
     out_RGB1 = cv2.VideoWriter(aviPath_RGB1, cv2.VideoWriter_fourcc(*'DIVX'), frameRate, size_RGB, True)
-    out_RGB2 = cv2.VideoWriter(aviPath_RGB2, cv2.VideoWriter_fourcc(*'DIVX'), frameRate, size_RGB, True)
-    out_RGB3 = cv2.VideoWriter(aviPath_RGB3, cv2.VideoWriter_fourcc(*'DIVX'), frameRate, size_RGB, True)
 
     i = 0
     while not rospy.is_shutdown():
-        if not ("nowImg_RGB1"  in globals() and "nowImg_RGB2"  in globals() and "nowImg_RGB3"  in globals()):
+        if not ("nowImg_RGB1"  in globals()):
             continue
 
         out_RGB1.write(nowImg_RGB1)
-        out_RGB2.write(nowImg_RGB2)
-        out_RGB3.write(nowImg_RGB3)
         if savePic:
             cv2.imwrite(os.path.join(jpgRoot_RGB1, "RGB1_{0:07}.jpg".format(i)), nowImg_RGB1)
-            cv2.imwrite(os.path.join(jpgRoot_RGB2, "RGB2_{0:07}.jpg".format(i)), nowImg_RGB2)
-            cv2.imwrite(os.path.join(jpgRoot_RGB3, "RGB3_{0:07}.jpg".format(i)), nowImg_RGB3)
             i += 1
 
         rate.sleep()
@@ -107,23 +79,17 @@ if __name__ == "__main__":
 
     if args.o:
         aviPath_RGB1 = os.path.join(root, "RGB1_"+args.o+".avi" )
-        aviPath_RGB2 = os.path.join(root, "RGB2_"+args.o+".avi" )
-        aviPath_RGB3 = os.path.join(root, "RGB3_"+args.o+".avi" )
 
         if os.path.exists(aviPath_RGB1):
             raise FileExistsError
 
         if args.storePic:
             jpgRoot_RGB1 = os.path.join(root, "IMG_RGB1_"+args.o)
-            jpgRoot_RGB2 = os.path.join(root, "IMG_RGB2_"+args.o)
-            jpgRoot_RGB3 = os.path.join(root, "IMG_RGB3_"+args.o)
 
     if args.storePic:
         savePic = True
         try:
             os.makedirs(jpgRoot_RGB1)
-            os.makedirs(jpgRoot_RGB2)
-            os.makedirs(jpgRoot_RGB3)
         except Exception:
             pass
 
