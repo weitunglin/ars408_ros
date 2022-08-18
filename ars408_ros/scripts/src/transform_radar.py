8bc66e4b20d5b9351ec7de0e557fba4ff2e8c385
weitung.lin <weitung8@gmail.com>
2022-08-19 00:46:36 +0800

    fixed transform_radar.py flashing rviz points

--- ars408_ros/scripts/src/transform_radar.py
+++ ars408_ros/scripts/src/transform_radar.py
@@ -9,15 +9,35 @@ import rospy
 import numpy as np
 
 from ars408_ros import radar_config, default_config
-from ars408_msg.msg import RadarPoints
+from ars408_msg.msg import RadarPoints, RadarPoint
 
 
 class RadarTransformer():
     def __init__(self):
         self.sub_decoded = defaultdict()
         self.radar_points = defaultdict()
+        self.radar_matrices = defaultdict()
         for radar_name in radar_config.names:
             self.sub_decoded[radar_name] = rospy.Subscriber("/radar/" + radar_name + "/decoded_messages", RadarPoints, partial(self.radar_callback, radar_name), queue_size=1)
+
+            # set up tranformation matrix for each radar
+            """
+            Matrix
+            [cos, -sin, tx]
+            [sin, cos,  ty]
+            [0,      0, 1]
+            """
+            self.radar_matrices[radar_name] = defaultdict()
+            x_translate, y_translate, rotate = radar_config[radar_name].transform
+            self.radar_matrices[radar_name]["rotate_dist"] = np.array([[math.cos(rotate), -1 * math.sin(rotate), 0],
+                                        [math.sin(rotate), math.cos(rotate), 0],
+                                        [0, 0, 1]])
+            self.radar_matrices[radar_name]["translate_dist"] = np.array([[1, 0, x_translate],
+                                    [0, 1, y_translate],
+                                    [0, 0, 1]])
+            self.radar_matrices[radar_name]["transform_dist"] = np.dot(self.radar_matrices[radar_name]["rotate_dist"], self.radar_matrices[radar_name]["translate_dist"])
+            self.radar_matrices[radar_name]["transform_vrel"] = np.array([[math.cos(rotate), -1 * math.sin(rotate)],
+                                [math.sin(rotate), math.cos(rotate)],])
         
         self.pub_transformed = rospy.Publisher("/radar/transformed_messages", RadarPoints, queue_size=1)
 
@@ -28,36 +48,30 @@ class RadarTransformer():
         transformed_radar_points = RadarPoints()
 
         for radar_name in self.radar_points:
-            x_translate, y_translate, rotate = radar_config[radar_name].transform
             for i in self.radar_points[radar_name].rps:
-                """
-                Matrix
-                [cos, -sin, tx]
-                [sin, cos,  ty]
-                [0,      0, 1]
-                """
-                mat_rotate_dist = np.array([[math.cos(rotate), -1 * math.sin(rotate), 0],
-                                            [math.sin(rotate), math.cos(rotate), 0],
-                                            [0, 0, 1]])
-                mat_trans_dist = np.array([[1, 0, x_translate],
-                                        [0, 1, y_translate],
-                                        [0, 0, 1]])
                 src_dist = np.array([i.distX, i.distY, 1])
-                mat_vrel = np.array([[math.cos(rotate), -1 * math.sin(rotate)],
-                                    [math.sin(rotate), math.cos(rotate)],])
                 src_vrel = np.array([i.vrelX, i.vrelY])
                 
-                dist = np.dot(np.dot(src_dist, mat_rotate_dist), mat_trans_dist)
-                # dist = np.dot(np.dot(src_dist, mat_trans_dist), mat_rotate_dist)
+                # dist = np.dot(np.dot(src_dist, mat_rotate_dist), mat_trans_dist)
+                dist = np.dot(src_dist, self.radar_matrices[radar_name]["transform_dist"])
                 
-                vrel = np.dot(src_vrel, mat_vrel)
+                vrel = np.dot(src_vrel, self.radar_matrices[radar_name]["transform_vrel"])
                 
-                i.distX = dist[0]
-                i.distY = dist[1]
-                i.angle = math.atan2(i.distY, i.distX)
-                i.vrelX = vrel[0]
-                i.vrelY = vrel[1]
-                transformed_radar_points.rps.append(i)
+                transformed_radar_points.rps.append(RadarPoint(
+                    id=i.id,
+                    dynProp=i.dynProp,
+                    distX=dist[0],
+                    distY=dist[1],
+                    vrelX=vrel[0],
+                    vrelY=vrel[1],
+                    rcs=i.rcs,
+                    strs=i.strs,
+                    prob=i.prob,
+                    classT=i.classT,
+                    angle=math.atan2(i.distX, i.distY),
+                    width=i.width,
+                    height=i.height
+                ))
         
         self.pub_transformed.publish(transformed_radar_points)
 
