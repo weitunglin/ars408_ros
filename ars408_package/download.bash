df5111d8ff1131cdd6f949954f942dc28d01e665
weitunglin <weitung8@gmail.com>
2022-08-27 05:26:46 +0000

    added download NVS weights in download.bash

--- ars408_package/download.bash
+++ ars408_package/download.bash
@@ -12,3 +12,16 @@ echo "REMOVE"
 rm weights.zip
 rm -r Micromax_torchv4_weight/
 echo "DONE."
+
+echo "Downloading NVS weights"
+wget https://www.dropbox.com/s/5yp1ft17v1155nj/weights.zip?dl=0 -O weights.zip
+unzip weights.zip
+
+echo "Moving best.pt to NVS/inference/weights"
+mkdir -p NVS/inference/weights
+cp weights/best.cp NVS/inference/weights/
+
+echo "Removing temp files"
+rm weights.zip
+rm -r weights/
+echo "Done"
