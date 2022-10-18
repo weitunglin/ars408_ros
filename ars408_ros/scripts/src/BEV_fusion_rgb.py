d032868e50c667837794be29fbec0f906f5eff7d
vip_laptop <vip@mail.ntust.edu.tw>
2022-10-18 14:50:20 +0000

    BEV update

--- ars408_ros/scripts/src/BEV_fusion_rgb.py
+++ ars408_ros/scripts/src/BEV_fusion_rgb.py
@@ -20,12 +20,6 @@ class RGBBEVFusion():
         self.config = rgb_config.names
         self.bridge = CvBridge()
 
-        # camera_matrix, valid_roi = cv2.getOptimalNewCameraMatrix(self.config.intrinsic_matrix, self.config.distortion_matrix, self.config.size, 1, self.config.size)
-        # self.camera_matrix = camera_matrix
-        # self.valid_roi = valid_roi
-
-        # new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(self.config.intrinsic_matrix, self.config.distortion_matrix, self.config.size, self.config.R, balance=1)
-        # self.mapx, self.mapy = cv2.fisheye.initUndistortRectifyMap(self.config.intrinsic_matrix, self.config.distortion_matrix, self.config.R, new_K, self.config.size, 5)
         """
         setup synchronizer for all rgb and radar pair.
         """
@@ -57,95 +51,23 @@ class RGBBEVFusion():
             5: rear_left
             '''
             rgb_image_array.append(self.bridge.imgmsg_to_cv2(msgs[i]))
-            # print(i, rgb_image_array[i].shape[0], rgb_image_array[i].shape[1])
+        
+        # Crop to fusion
+        rgb_image_array[0] = rgb_image_array[0][0:1000,0:712]
+        rgb_image_array[2] = rgb_image_array[2][0:1000,0:712]
 
-        '''
-        rot_front_left = np.rot90(rgb_image_array[0], 2)
-        # rot_front_center = np.rot90(rgb_image_array[1], 0)
-        rot_front_right = np.rot90(rgb_image_array[2], 2)
-        # rot_rear_right = np.rot90(rgb_image_array[3], 0)
-        rot_rear_center = np.rot90(rgb_image_array[4], 2)
-        # rot_rear_left = np.rot90(rgb_image_array[5], 0)
-        # rot_rearCenter = np.rot90(rgb_image_array[4], 2)
+        right = cv2.vconcat([rgb_image_array[2],rgb_image_array[3]])
+        left = cv2.vconcat([rgb_image_array[0], rgb_image_array[5]])
 
-        image_result = cv2.vconcat([rgb_image_array[1], rot_rear_center])
-        '''
+        fusion_result = cv2.copyMakeBorder(left,0,0,0,500,cv2.BORDER_CONSTANT,value=[0,0,0])
+        fusion_result = cv2.hconcat([fusion_result,right])
 
-        rot_front_left = np.rot90(rgb_image_array[0], 1)
-        rot_front_center = rgb_image_array[1]    #np.rot90(rgb_image_array[1], 0)
-        rot_front_right = np.rot90(rgb_image_array[2], -1)
-        rot_rear_right = np.rot90(rgb_image_array[3], -1)
-        rot_rear_center = np.rot90(rgb_image_array[4], 2)
-        rot_rear_left = np.rot90(rgb_image_array[5], 1)
-
-        img_High = rot_front_center.shape[0]
-        img_Width = rot_front_center.shape[1]
-
-        src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-        dst = np.float32([[0 + 50, img_High], [img_Width + 50, img_High], [0 + 50, 0], [img_Width + 50, 0]])
-        M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-        warped_img_front_center = cv2.warpPerspective(rot_front_center, M, (1280 + 100, 2260)) # Image warping
-
-
-        rot_front_left = rot_front_left[0:rot_front_left.shape[0] - 200, 0:rot_front_left.shape[1]]
-        img_High = rot_front_center.shape[0]
-        img_Width = rot_front_center.shape[1]
-
-        src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-        dst = np.float32([[0, img_High + 50], [img_Width, img_High + 50], [0, 0 + 50], [img_Width, 0 + 50]])
-        M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-        warped_img_front_left = cv2.warpPerspective(rot_front_left, M, (1280 + 100, 2260)) # Image warping
-
-        rot_rear_left = rot_rear_left[200:rot_rear_left.shape[0], 0:rot_rear_left.shape[1]]
-        img_High = rot_rear_left.shape[0]
-        img_Width = rot_rear_left.shape[1]
-
-        src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-        dst = np.float32([[0, img_High + 1130], [img_Width, img_High + 1130], [0, 0 + 1130], [img_Width, 0 + 1130]])
-        M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-        warped_img_rear_left = cv2.warpPerspective(rot_rear_left, M, (1280 + 100, 2260)) # Image warping
-
-        img_High = rot_rear_center.shape[0]
-        img_Width = rot_rear_center.shape[1]
-
-        src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-        dst = np.float32([[0 + 50, img_High + 2020], [img_Width + 50, img_High + 2020], [0 + 50, 0 + 2020], [img_Width + 50, 0 + 2020]])
-        M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-        warped_img_rear_center = cv2.warpPerspective(rot_rear_center, M, (1280 + 100, 2260)) # Image warping
-
-
-        rot_front_right = rot_front_right[0:rot_front_right.shape[0] - 200, 0:rot_front_right.shape[1]]
-        img_High = rot_front_right.shape[0]
-        img_Width = rot_front_right.shape[1]
-
-        src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-        dst = np.float32([[0 + 1135, img_High + 50], [img_Width + 1135, img_High + 50], [0 + 1135, 0 + 50], [img_Width + 1135, 0 + 50]])
-        M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-        warped_img_front_right = cv2.warpPerspective(rot_front_right, M, (1280 + 100, 2260)) # Image warping
-
-        rot_rear_right = rot_rear_right[200:rot_rear_right.shape[0], 0:rot_rear_right.shape[1]]
-        img_High = rot_rear_right.shape[0]
-        img_Width = rot_rear_right.shape[1]
-
-        src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-        dst = np.float32([[0 + 1135, img_High + 1130], [img_Width + 1135, img_High + 1130], [0 + 1135, 0 + 1130], [img_Width + 1135, 0 + 1130]])
-        M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-        warped_img_rear_right = cv2.warpPerspective(rot_rear_right, M, (1280 + 100, 2260)) # Image warping
-
-
-        image_result = cv2.add(warped_img_front_center, warped_img_front_left)
-        image_result = cv2.add(image_result, warped_img_rear_left)
-        image_result = cv2.add(image_result, warped_img_rear_center)
-        image_result = cv2.add(image_result, warped_img_front_right)
-        image_result = cv2.add(image_result, warped_img_rear_right)
-
-        msg = self.bridge.cv2_to_imgmsg(image_result)
+        top = cv2.copyMakeBorder(rgb_image_array[1],0,0,322,322,cv2.BORDER_CONSTANT,value=[0,0,0])
+        bottom = cv2.copyMakeBorder(rgb_image_array[4],0,0,322,322,cv2.BORDER_CONSTANT,value=[0,0,0])
+ 
+        fusion_result = cv2.vconcat([top,fusion_result])
+        fusion_result = cv2.vconcat([fusion_result,bottom])
+        msg = self.bridge.cv2_to_imgmsg(fusion_result)
         # msg.header.stamp = rospy.Time.now()
         msg.header = Header(stamp=rospy.Time.now())
         self.pub_calib_rgb.publish(msg)
@@ -154,15 +76,7 @@ class RGBBEVFusion():
 
 if __name__ == "__main__":
     try:
-        # if len(sys.argv) < 2:
-        #     rospy.logerr("error getting rgb_name. got {}".format(",".join(sys.argv)))
-        #     exit(-1)
-        # else:
-        #     rgb_name = sys.argv[1]
-
-        rospy.init_node("RGB BEV Fusion")
-
-        
+        rospy.init_node("RGB BEV Fusion")    
         rgb_to_ros = RGBBEVFusion()
         rospy.spin()
 
--- ars408_ros/scripts/src/BEV_rgb.py
+++ ars408_ros/scripts/src/BEV_rgb.py
@@ -2,6 +2,8 @@
 # coding=utf-8
 import sys
 
+from cv2 import ROTATE_180, ROTATE_90_CLOCKWISE, ROTATE_90_COUNTERCLOCKWISE
+
 import rospy
 import cv2
 import numpy as np
@@ -13,28 +15,52 @@ from config.config import rgb_config
 
 import os
 
+
+
+
+
+
 class RGBBEV():
     def __init__(self, rgb_name):
+
+        def get_perspective_matrix():
+            FL_src = np.float32([[200, 410], [900, 410], [-300, 700], [880, 700]])
+            FL_dst = np.float32([[100, 200], [1280, 100], [500, 650], [880, 650]])
+
+            FC_src = np.float32([[0, 712], [1280, 712], [0, 0], [1280, 0]])
+            FC_dst = np.float32([[1280 / 2 - 65, 712], [1280 / 2 + 65, 712], [0, 0], [1280, 0]])
+
+            FR_src = np.float32([[550, 410], [990, 410], [0, 650], [1540, 650]])
+            FR_dst = np.float32([[0, 0], [1540, 0], [0, 650], [1540, 650]])
+
+            RR_src = np.float32([[580, 400], [925, 400], [400, 700], [1105, 700]])
+            RR_dst = np.float32([[400, 0], [1105, 0], [400, 700], [1105, 700]])
+
+            RC_src = np.float32([[0, 712], [1280, 712], [0, 0], [1280, 0]])
+            RC_dst = np.float32([[1280 / 2 - 65, 712], [1280 / 2 + 65, 712], [0, 0], [1280, 0]])
+
+            RL_src = np.float32([[430, 430], [850, 430], [-230, 710], [1280, 710]])
+            RL_dst = np.float32([[-200, 0], [1480, 0], [-230, 710], [1280,710]])
+
+            return [cv2.getPerspectiveTransform(FL_src,FL_dst),
+            cv2.getPerspectiveTransform(FC_src,FC_dst),
+            cv2.getPerspectiveTransform(FR_src,FR_dst),
+            cv2.getPerspectiveTransform(RR_src,RR_dst),
+            cv2.getPerspectiveTransform(RC_src,RC_dst),
+            cv2.getPerspectiveTransform(RL_src,RL_dst)]
+            
         self.rgb_name = rgb_name
         self.config = rgb_config[self.rgb_name]
         self.bridge = CvBridge()
-
-        # camera_matrix, valid_roi = cv2.getOptimalNewCameraMatrix(self.config.intrinsic_matrix, self.config.distortion_matrix, self.config.size, 1, self.config.size)
-        # self.camera_matrix = camera_matrix
-        # self.valid_roi = valid_roi
-
-        # new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(self.config.intrinsic_matrix, self.config.distortion_matrix, self.config.size, self.config.R, balance=1)
-        # self.mapx, self.mapy = cv2.fisheye.initUndistortRectifyMap(self.config.intrinsic_matrix, self.config.distortion_matrix, self.config.R, new_K, self.config.size, 5)
-
+        self.perspective_matrix = get_perspective_matrix()
+        # print(self.perspective_matrix)
         self.sub_rgb = rospy.Subscriber("calib_image", Image, self.callback, queue_size=1)
         self.pub_BEV_rgb = rospy.Publisher("BEV_image", Image, queue_size=1)
 
+    
+
     def callback(self, data):
         img = self.bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
-
-        # 1280 * 712
-        img_Width = 1280
-        img_High = 712
 
         '''
             0: front_left
@@ -44,125 +70,34 @@ class RGBBEV():
             4: rear_center
             5: rear_left
         '''
-        # print(self.rgb_name)
-
-        ''' #for Tasla camera
-        if(self.rgb_name == 'front_left'):
-            img = img[450:img_High - 20 ,350:img_Width]
-            
-            src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-            dst = np.float32([[img_Width / 2 - 65, img_High], [img_Width / 2 + 65, img_High], [0, 0], [img_Width, 0]])
-            M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-            warped_img = cv2.warpPerspective(img, M, (img_Width - 350, img_High - 450 - 20)) # Image warping
-
-        elif(self.rgb_name == 'front_center'):
-            img = img[430:img_High - 20 ,0:img_Width]
-            # img = img[450:img_High - 20 ,0:img_Width]
-            
-            src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-            dst = np.float32([[img_Width / 2 - 65, img_High], [img_Width / 2 + 65, img_High], [0, 0], [img_Width, 0]])
-            M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-            warped_img = cv2.warpPerspective(img, M, (img_Width, img_High - 430 - 20)) # Image warping
-            # warped_img = cv2.warpPerspective(img, M, (img_Width, img_High - 450 - 20)) # Image warping
-
-        elif(self.rgb_name == 'front_right'):
-            img = img[450:img_High - 20 ,0:img_Width - 350]
-            
-            src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-            dst = np.float32([[img_Width / 2 - 65, img_High], [img_Width / 2 + 65, img_High], [0, 0], [img_Width, 0]])
-            M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-            warped_img = cv2.warpPerspective(img, M, (img_Width - 350, img_High - 450 - 20)) # Image warping
-
-        elif(self.rgb_name == 'rear_right'):
-            img = img[550:img_High - 20 ,350:img_Width]
-            
-            src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-            dst = np.float32([[img_Width / 2 - 65, img_High], [img_Width / 2 + 65, img_High], [0, 0], [img_Width, 0]])
-            M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-            warped_img = cv2.warpPerspective(img, M, (img_Width - 350, img_High - 550 - 20)) # Image warping
-
-        elif(self.rgb_name == 'rear_center'):
-            img = img[450:img_High - 20 ,0:img_Width]
-            # img = img[550:img_High - 20 ,0:img_Width]
-            
-            src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-            dst = np.float32([[img_Width / 2 - 65, img_High], [img_Width / 2 + 65, img_High], [0, 0], [img_Width, 0]])
-            M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-            warped_img = cv2.warpPerspective(img, M, (img_Width, img_High - 450 - 20)) # Image warping
-            # warped_img = cv2.warpPerspective(img, M, (img_Width, img_High - 550 - 20)) # Image warping
-
-        elif(self.rgb_name == 'rear_left'):
-            # img = img[550:img_High - 0 ,0:img_Width - 350]
-            img = img[550:img_High - 20 ,0:img_Width - 350]
-            
-            src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-            dst = np.float32([[img_Width / 2 - 65, img_High], [img_Width / 2 + 65, img_High], [0, 0], [img_Width, 0]])
-            M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-            # warped_img = cv2.warpPerspective(img, M, (img_Width - 350, img_High - 550 - 0)) # Image warping
-            warped_img = cv2.warpPerspective(img, M, (img_Width - 350, img_High - 550 - 20)) # Image warping
-        '''
-
+        
         # for Original camera
         if(self.rgb_name == 'front_left'):
-            img = img[450:img_High - 20 ,0:img_Width]
-        
-            src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-            dst = np.float32([[img_Width / 2 - 65, img_High], [img_Width / 2 + 65, img_High], [0, 0], [img_Width, 0]])
-            M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-            warped_img = cv2.warpPerspective(img, M, (img_Width, img_High - 450 - 20)) # Image warping
+            img = cv2.copyMakeBorder(img,0,0,0,1000,cv2.BORDER_CONSTANT,value=[0,0,0])
+            warped_img = cv2.warpPerspective(img, self.perspective_matrix[0], (1280,712)) 
+            warped_img = cv2.rotate(warped_img,ROTATE_90_COUNTERCLOCKWISE)
 
         elif(self.rgb_name == 'front_center'):
-            img = img[450:img_High - 20 ,0:img_Width]
-        
-            src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-            dst = np.float32([[img_Width / 2 - 65, img_High], [img_Width / 2 + 65, img_High], [0, 0], [img_Width, 0]])
-            M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-            warped_img = cv2.warpPerspective(img, M, (img_Width, img_High - 450 - 20)) # Image warping
-
+            img = img[450:712 - 20 ,0:1280]
+            warped_img = cv2.warpPerspective(img, self.perspective_matrix[1], (1280, 712 - 450 - 20)) 
 
         elif(self.rgb_name == 'front_right'):
-            img = img[450:img_High - 20 ,0:img_Width]
-        
-            src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-            dst = np.float32([[img_Width / 2 - 65, img_High], [img_Width / 2 + 65, img_High], [0, 0], [img_Width, 0]])
-            M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-            warped_img = cv2.warpPerspective(img, M, (img_Width, img_High - 450 - 20)) # Image warping
+            img = cv2.copyMakeBorder(img,0,0,0,1000,cv2.BORDER_CONSTANT,value=[0,0,0])
+            warped_img = cv2.warpPerspective(img, self.perspective_matrix[2], (1280,712)) 
+            warped_img = cv2.rotate(warped_img,ROTATE_90_CLOCKWISE)
 
         elif(self.rgb_name == 'rear_right'):
-            img = img[450:img_High - 20 ,0:img_Width]
-        
-            src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-            dst = np.float32([[img_Width / 2 - 65, img_High], [img_Width / 2 + 65, img_High], [0, 0], [img_Width, 0]])
-            M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-            warped_img = cv2.warpPerspective(img, M, (img_Width, img_High - 450 - 20)) # Image warping
+            warped_img = cv2.warpPerspective(img, self.perspective_matrix[3], (1280,712)) 
+            warped_img = cv2.rotate(warped_img,ROTATE_90_CLOCKWISE)
 
         elif(self.rgb_name == 'rear_center'):
-            img = img[450:img_High - 20 ,0:img_Width]
-        
-            src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-            dst = np.float32([[img_Width / 2 - 65, img_High], [img_Width / 2 + 65, img_High], [0, 0], [img_Width, 0]])
-            M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-            warped_img = cv2.warpPerspective(img, M, (img_Width, img_High - 450 - 20)) # Image warping
+            img = img[450:712 - 20 ,0:1280]
+            warped_img = cv2.warpPerspective(img, self.perspective_matrix[4], (1280, 712 - 450 - 20)) 
+            warped_img = cv2.rotate(warped_img,ROTATE_180)
 
         elif(self.rgb_name == 'rear_left'):
-            img = img[450:img_High - 20 ,0:img_Width]
-        
-            src = np.float32([[0, img_High], [img_Width, img_High], [0, 0], [img_Width, 0]])
-            dst = np.float32([[img_Width / 2 - 65, img_High], [img_Width / 2 + 65, img_High], [0, 0], [img_Width, 0]])
-            M = cv2.getPerspectiveTransform(src, dst) # The transformation matrix
-
-            warped_img = cv2.warpPerspective(img, M, (img_Width, img_High - 450 - 20)) # Image warping
+            warped_img = cv2.warpPerspective(img, self.perspective_matrix[5], (1280,712)) 
+            warped_img = cv2.rotate(warped_img,ROTATE_90_COUNTERCLOCKWISE)
 
         msg = self.bridge.cv2_to_imgmsg(warped_img)
         # msg.header.stamp = rospy.Time.now()
@@ -178,8 +113,6 @@ if __name__ == "__main__":
             rgb_name = sys.argv[1]
 
         rospy.init_node("RGB BEV")
-
-        
         rgb_to_ros = RGBBEV(rgb_name)
         rospy.spin()
 
