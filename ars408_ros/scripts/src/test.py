c1dbb3ca9890053a8a28b4298049c7c560cdd56b
vip_laptop <vip@mail.ntust.edu.tw>
2023-01-11 01:58:37 +0800

    AEB algorithm updated

--- ars408_ros/scripts/src/test.py
+++
@@ -1,9 +0,0 @@
-lis = [{"id":1, "name":"a"},{"id":2, "name":"b"},{"id":3, "name":"c"},{"id":4, "name":"d"}]
-
-print(lis)
-
-if(next((i for i in lis if i['id'] == 1), None) != None):
-    lis.remove(next(i for i in lis if i['id'] == 1))
-
-
-print(lis)
